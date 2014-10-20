562a9397edace265e93dc1236a5eaeb2e6d5923c
Batnasan Byambasuren <batnasan06@gmail.com>
2014-10-20 23:04:07 +0800

    BBC radio stations added

--- plugin.audio.enstations/addon.py
+++ plugin.audio.enstations/addon.py
@@ -8,6 +8,12 @@ plugin = Plugin()
 def index():
     item = [
             {'label': 'Voice of America', 'path': 'http://voa-28.akacast.akamaistream.net/7/54/322040/v1/ibb.akacast.akamaistream.net/voa-28', 'is_playable': True,},
+            {'label': 'BBC World Service', 'path': 'http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-eieuk', 'is_playable': True,},
+            {'label': 'BBC 1', 'path': 'http://www.bbc.co.uk/radio/listen/live/r1.asx', 'is_playable': True,},
+            {'label': 'BBC 2', 'path': 'http://www.bbc.co.uk/radio/listen/live/r2.asx', 'is_playable': True,},
+            {'label': 'BBC 4', 'path': 'http://www.bbc.co.uk/radio/listen/live/r4lw.asx', 'is_playable': True,},
+            {'label': 'BBC 5', 'path': 'http://www.bbc.co.uk/radio/listen/live/r5l.asx', 'is_playable': True,},
+            {'label': 'BBC 6 Music', 'path': 'mms://wmlive-nonacl.bbc.net.uk/wms/bbc_ami/6music/6music_bb_live_int_eq1_sw1', 'is_playable': True,},
     ]
     return item
 
